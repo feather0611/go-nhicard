ad4b285ff8a3ecdd40e38ecba9ca86c47e5e9ccb
Nick Lin <feather0611@icloud.com>
2022-12-04 01:19:45 +0800

    add some more apis.

--- api.go
+++ api.go
@@ -4,19 +4,45 @@ import (
 	"syscall"
 )
 
-func dllImport(dllPath string ,functionName string) *syscall.LazyProc {
-	handle := syscall.NewLazyDLL(dllPath)
-	dllLib := handle.NewProc(functionName)
-	return dllLib
+type Nhilib struct {
+	path    string
+	handler *syscall.LazyDLL
+	proc    *syscall.LazyProc
 }
 
-func hisGetBasicData() (int, []byte) {
-	dllFunction := dllImport("hisGetBasicData")
+func SetDll(path string) *Nhilib {
+	lib := new(Nhilib)
+	lib.path = path
+	lib.handler = syscall.NewLazyDLL(path)
+	return lib
+}
+
+func (l *Nhilib) SetFunction(functionName string) {
+	l.proc = l.handler.NewProc(functionName)
+}
+
+func (l *Nhilib) GetBasicData() (uintptr, []byte) {
+	l.SetFunction("hisGetBasicData")
 	pBuffer := make([]byte, 72)
-	errorCode, _, _ := dllFunction.Call(BytePtr(pBuffer), Int2IntPtr(72))
+	errorCode, _, _ := l.proc.Call(BytePtr(pBuffer), Int2IntPtr(72))
 	return errorCode, pBuffer
 }
 
-func csOpenCom(p int) {
+func (l *Nhilib) OpenCom(p int) uintptr {
+	l.SetFunction("csOpenCom")
+	errorCode, _, _ := l.proc.Call(Int2IntPtr(p))
+	return errorCode
+}
 
+func (l *Nhilib) CloseCom() uintptr {
+	l.SetFunction("csCloseCom")
+	errorCode, _, _ := l.proc.Call()
+	return errorCode
+}
+
+func (l *Nhilib) GetDllVersion() uintptr {
+	l.SetFunction("csGetVersionEx")
+	addr := []byte(l.path)
+	returnCode, _, _ := l.proc.Call(BytePtr(addr))
+	return returnCode
 }
