e5564c26724530cb8f83cc513b5f2861ee99dc62
Nick Lin <feather0611@icloud.com>
2022-12-04 02:12:20 +0800

    Change Big5toUTF8 from private to public.

--- big5Transfer.go
+++ big5Transfer.go
@@ -5,7 +5,7 @@ import (
 	"golang.org/x/text/transform"
 )
 
-func big5ToUTF8(src []byte) string {
+func Big5ToUTF8(src []byte) string {
 	decoder := traditionalchinese.Big5.NewDecoder()
 	result, _, _ := transform.String(decoder, string(src))
 	return result
